58b4ec75528a8a73aeb5f341630a705931ec0a8f
ashleyshubin <86271205+ashleyshubin@users.noreply.github.com>
2023-02-05 17:14:11 -0800

    print date

--- src/pcc/cis/helloworld/HelloWorld.java
+++ src/pcc/cis/helloworld/HelloWorld.java
@@ -1,7 +1,8 @@
 package pcc.cis.helloworld;
-
+import java.util.Date;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        System.out.println(new Date());
     }
 }
